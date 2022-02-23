68bd1c27e2b22f89a853a6a3c46ba65302ed1c67
Duane Cilliers <hello@duane.co.za>
2022-02-23 22:34:04 +0200

    add ghost button variant

--- libs/ui/src/lib/button/button.tsx
+++ libs/ui/src/lib/button/button.tsx
@@ -4,11 +4,24 @@ import '../../styles/index.css';
 /* eslint-disable-next-line */
 export interface ButtonProps {
   children: ReactNode;
+  variant?: 'primary' | 'ghost';
 }
 
-export function Button({ children }: ButtonProps) {
+export function Button({ children, variant = 'primary' }: ButtonProps) {
   return (
-    <button className="bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 active:bg-blue-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white transition-all">
+    <button
+      className={`
+      focus:outline-none focus:ring py-2 px-5 text-sm leading-5 rounded-full font-semibold transition-all
+      ${
+        variant === 'primary' &&
+        ' bg-blue-400 hover:bg-blue-500 focus:ring-blue-200 active:bg-blue-600 text-white'
+      }
+      ${
+        variant === 'ghost' &&
+        ' bg-transparent border-2 border-blue-400 text-blue-400 hover:text-blue-500 active:text-blue-600'
+      }
+      `}
+    >
       {children}
     </button>
   );
