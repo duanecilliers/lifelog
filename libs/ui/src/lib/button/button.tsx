a9e38fe1dcf05762f983f002cf05c76ee8028145
Duane Cilliers <hello@duane.co.za>
2022-02-23 22:23:37 +0200

    configure tailwind and style button component

--- libs/ui/src/lib/button/button.tsx
+++ libs/ui/src/lib/button/button.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import './button.module.css';
+import '../../styles/index.css';
 
 /* eslint-disable-next-line */
 export interface ButtonProps {
@@ -7,7 +7,11 @@ export interface ButtonProps {
 }
 
 export function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+  return (
+    <button className="bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 active:bg-blue-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white transition-all">
+      {children}
+    </button>
+  );
 }
 
 export default Button;
