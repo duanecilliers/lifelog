499339eaf7385c448fd84ed6046f199892153bf8
Duane Cilliers <hello@duane.co.za>
2022-02-26 14:27:17 +0200

    refactor: move polymorphic link component to separate component

--- libs/ui/src/lib/life-calendar/life-calendar.tsx
+++ libs/ui/src/lib/life-calendar/life-calendar.tsx
@@ -1,40 +1,11 @@
 import { ElementType } from 'react';
 import { splitEvery, map, add, range } from 'ramda';
-import { PolymorphicComponentProps } from '../../types/PolymorphicComponentProps';
+import Link from './link';
 
 export interface LifeCalendarProps {
   age: number;
   linkElement?: ElementType;
 }
-
-interface Props {
-  age: number;
-  year: number;
-}
-
-type LinkProps<C extends ElementType> = PolymorphicComponentProps<C, Props>;
-
-const Link = <C extends ElementType = 'a'>({
-  as,
-  age,
-  year,
-  ...props
-}: LinkProps<C>) => {
-  const Component = as ?? 'a';
-  return (
-    <Component
-      {...props}
-      key={`age-${year}`}
-      className={`my-0.5 bg-transparent text-sm flex items-center justify-center rounded-full md:m-2 lg:m-4 xl:m-8 ${
-        age > year && ` text-gray-300`
-      }
-      ${age < year && ` text-gray-800`}
-      ${age === year && ` bg-blue-400 text-white`}`}
-    >
-      {year}
-    </Component>
-  );
-};
 
 export function LifeCalendar({ age, linkElement }: LifeCalendarProps) {
   const groupedYears = splitEvery(10, map(add(1), range(0, 100)));
---
+++ libs/ui/src/lib/life-calendar/link.tsx
@@ -0,0 +1,33 @@
+import { ElementType } from 'react';
+import { PolymorphicComponentProps } from '../../types/PolymorphicComponentProps';
+
+interface Props {
+  age: number;
+  year: number;
+}
+
+type LinkProps<C extends ElementType> = PolymorphicComponentProps<C, Props>;
+
+const Link = <C extends ElementType = 'a'>({
+  as,
+  age,
+  year,
+  ...props
+}: LinkProps<C>) => {
+  const Component = as ?? 'a';
+  return (
+    <Component
+      {...props}
+      key={`age-${year}`}
+      className={`my-0.5 bg-transparent text-sm flex items-center justify-center rounded-full md:m-2 lg:m-4 xl:m-8 ${
+        age > year && ` text-gray-300`
+      }
+      ${age < year && ` text-gray-800`}
+      ${age === year && ` bg-blue-400 text-white`}`}
+    >
+      {year}
+    </Component>
+  );
+};
+
+export default Link;
